e13dd993a73d92d65829bf7601c7b6fc32ae9057
Jenna Goldman <jenna.goldman89@gmail.com>
2020-04-01 18:44:24 -0700

    add POST and GET ALL routes for posts

---
+++ __tests__/posts.test.js
@@ -0,0 +1,84 @@
+const { getUser, getAgent, getPost, getPosts } = require('../db/data-helpers');
+
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('posts routes', () => {
+  it('creates a post', async() => {
+    const user = await getUser();
+
+    return getAgent()
+      .post('/api/v1/posts')
+      .send({
+        user: user._id,
+        photoUrl: 'http://placekitten.com/200/200',
+        caption: 'A cute kitten.',
+        tags: ['cats', 'kitten', 'icanhazcheeseburger']
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          user: user._id,
+          photoUrl: 'http://placekitten.com/200/200',
+          caption: 'A cute kitten.',
+          tags: ['cats', 'kitten', 'icanhazcheeseburger'],
+          __v: 0
+        });
+      });
+  });
+
+  it('gets all posts', async() => {
+    const posts = await getPosts();
+
+    return request(app)
+      .get('/api/v1/posts')
+      .then(res => {
+        posts.forEach(post => {
+          expect(res.body).toContainEqual({
+            _id: post._id,
+            user: post.user,
+            photoUrl: post.photoUrl,
+            caption: post.caption,
+            tags: expect.any(Array),
+            __v: 0
+          });
+        });
+      });
+  });
+
+  it('gets a post by id', async() => {
+    const post = await getPost();
+
+    return request(app)
+      .get(`/api/v1/posts/${post._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: post._id,
+          user: {
+            _id: expect.any(String),
+            profilePhotoUrl: expect.any(String),
+            username: expect.any(String),
+            __v: 0
+          },
+          photoUrl: post.photoUrl,
+          caption: post.caption,
+          tags: expect.any(Array),
+          __v: 0
+        });
+      });
+  });
+
+  // it('updates a post by id', async() => {
+  //   const post = await getPost();
+
+  //   return request(app)
+  //     .patch(`/api/v1/posts/${post._id}`)
+  //     .send({ caption: 'this is a new caption' })
+  //     .then(res => {
+  //       expect(res.body).toEqual({
+  //         ...post,
+  //         caption: 'this is a new caption'
+  //       });
+  //     });
+  // });
+});
--- db/seed.js
+++ db/seed.js
@@ -1,7 +1,8 @@
 const chance = require('chance').Chance();
 const User = require('../lib/models/User');
+const Post = require('../lib/models/Post');
 
-module.exports = async({ usersToCreate = 5 } = {}) => {
+module.exports = async({ usersToCreate = 5, postsToCreate = 25 } = {}) => {
   const loggedInUser = await User.create({
     username: 'jennag',
     password: 'password',
@@ -12,5 +13,14 @@ module.exports = async({ usersToCreate = 5 } = {}) => {
     username: chance.email(),
     password: chance.animal(),
     profilePhotoUrl: `https://fakeimg.com/${chance.animal()}.jpg`
+  })));
+
+  const tags = ['tbt', 'smile', 'aww', 'feltcute', 'butfirstcoffee', 'dogstagram', 'blessed'];
+
+  const posts = await Post.create([...Array(postsToCreate)].slice(1).map(() => ({
+    user: chance.pickone(users)._id,
+    photoUrl: chance.url(),
+    caption: chance.sentence(),
+    tags: [chance.pickone(tags), chance.pickone(tags)]
   })));
 };
